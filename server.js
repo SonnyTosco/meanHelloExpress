9f28fe881b3581efb51416205238fec21ee06358
Sonny Tosco <sonny.tosco@gmail.com>
2016-10-08 20:53:08 -0700

    created the static dir

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ var express = require("express");
 
 // invoke var express and store the resulting application in var application
 var app = express();
-
+app.use(express.static(__dirname + "/static"));
+console.log(__dirname);
 //lets handle the base route '/' and respond with "Hello Express"
 app.get('/', function(request, response){
   response.send("<h1>Hello Express</h1>");
