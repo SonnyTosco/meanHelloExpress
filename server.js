fa825a0fbbb1edcae67dc0e900a4efe841a99100
Sonny Tosco <sonny.tosco@gmail.com>
2016-10-08 21:09:30 -0700

    added views and complete

--- server.js
+++ server.js
@@ -5,7 +5,12 @@ var express = require("express");
 // invoke var express and store the resulting application in var application
 var app = express();
 app.use(express.static(__dirname + "/static"));
-console.log(__dirname);
+
+// This sets the location where express will look for the ejs views
+app.set('views', __dirname + '/views');
+// Now lets set the view engine itself so that express knows that we are using ejs as opposed to another templating engine like jade
+app.set('view engine', 'ejs');
+
 //lets handle the base route '/' and respond with "Hello Express"
 app.get('/', function(request, response){
   response.send("<h1>Hello Express</h1>");
@@ -19,3 +24,13 @@ app.listen(8000, function(){
 })
 // this line will almost always be at the end of youor server.js file
 //(we only tell the server to listen after we have set up all of our rules)
+app.get("/users", function (request, response){
+    // hard-coded user data
+    var users_array = [
+        {name: "Michael", email: "michael@codingdojo.com"},
+        {name: "Jay", email: "jay@codingdojo.com"},
+        {name: "Brendan", email: "brendan@codingdojo.com"},
+        {name: "Andrew", email: "andrew@codingdojo.com"}
+    ];
+    response.render('users', {users: users_array});
+})
